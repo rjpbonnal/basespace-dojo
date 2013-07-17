b2aaa84d714d3bb3702f8c99cfd11320a317063d
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-07-17 19:33:36 +0200

    BaseSpace web app container...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webfoo::Application.config.secret_key_base = 'd27755a427dc18af90a2fed35e6ab75c33dc1dbe5abf600a32db2eca696615872ccccc9787cd89b0cf2317421f416cc7926f8a7fce6a94075807a116616834a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webfoo::Application.config.session_store :cookie_store, key: '_Webfoo_session'
