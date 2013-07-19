b370173ac9f37ae6b3d2e46073d1361085610e33
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-07-19 12:41:20 +0200

    kytm photo

--- app/controllers/aboutus_controller.rb
+++ app/controllers/aboutus_controller.rb
@@ -1,7 +1,7 @@
 class AboutusController < ApplicationController
   def index
   	@gitusers = [{"login" =>  "helios", "name" =>"Raoul Jean Pierre Bonnal", "avatar_url" =>"helios.jpeg"},
-  	             {"login" => "ktym", "name" =>"Toshiaki Katayama", "avatar_url" =>"ktym.png"},
+  	             {"login" => "ktym", "name" =>"Toshiaki Katayama", "avatar_url" =>"ktym.jpg"},
   	             {"login" => "joejimbo", "name" =>"Joachim Baran", "avatar_url" =>"joejimbo.jpeg"},
   	             {"login" => "fstrozzi", "name" =>"Francesco Strozzi", "avatar_url" => "fstrozzi.jpeg"},
   	             {"login" => "pjotrp", "name"  =>"Pjotr Prins", "avatar_url" =>"pjotrp.jpeg"}]
