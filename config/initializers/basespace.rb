303e5db67b315010f1af2d365070b20ce48a3231
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-07-19 17:16:22 +0200

    global configuration load at startup time.

--- config/initializers/basespace.rb
+++ config/initializers/basespace.rb
@@ -1,4 +1,6 @@
 #`export BASESPACE_CREDENTIALS=#{File.expand_path('.','credentials.json')}`
 require 'basespace'
+require 'json'
 include Bio::BaseSpace
-Webfoo::Application.config.basespace = Bio::BaseSpace.load_credentials
+# Webfoo::Application.config.basespace = Bio::BaseSpace.load_credentials
+Webfoo::Application.config.basespace = BaseSpaceAPI.start
