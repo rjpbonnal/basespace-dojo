f9d464479f90fe544068e4668f2307fee89951d6
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-07-19 14:10:03 +0200

    add Eri data

--- app/controllers/aboutus_controller.rb
+++ app/controllers/aboutus_controller.rb
@@ -3,6 +3,7 @@ class AboutusController < ApplicationController
   	@gitusers = [{"login" =>  "helios", "name" =>"Raoul Jean Pierre Bonnal", "avatar_url" =>"helios.jpeg"},
   	             {"login" => "ktym", "name" =>"Toshiaki Katayama", "avatar_url" =>"ktym.jpg"},
   	             {"login" => "joejimbo", "name" =>"Joachim Baran", "avatar_url" =>"joejimbo.jpeg"},
+  	             {"login" => "erikiv", "name" =>"Eri Kibukawa", "avatar_url" =>"octocat.png"},
   	             {"login" => "fstrozzi", "name" =>"Francesco Strozzi", "avatar_url" => "fstrozzi.jpeg"},
   	             {"login" => "pjotrp", "name"  =>"Pjotr Prins", "avatar_url" =>"pjotrp.jpeg"}]
   end
